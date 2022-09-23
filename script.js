1275483ebaafb550f4a589652859366f749f9468
Toacin <toacinp@outlook.com>
2022-09-22 19:21:10 -0700

    colors now display appropriately relative to current time of day

--- script.js
+++ script.js
@@ -15,8 +15,19 @@ let $3pmInput = $("#3pm");
 let $4pmInput = $("#4pm");
 let $5pmInput = $("#5pm");
 
+arrayof$Times = [$9amInput, $10amInput, $11amInput, $12pmInput, $1pmInput, $2pmInput, $3pmInput, $4pmInput, $5pmInput]
+
 $currentDay.text(currentDay);
 
-if (moment().isAfter(moment("8:00 AM", 'h:mm A'))) {
-    $9amInput.addClass("past");
+for (i=0; i<9; i++) {
+    if (moment("3 PM", 'h A').isAfter(moment("9 AM", 'h A').add(i, 'h'))) {
+        arrayof$Times[i].addClass("past");
+    } else if (moment("3 PM", 'h A').isSame(moment("9 AM", 'h A').add(i, 'h'))) {
+        arrayof$Times[i].addClass("present");
+    } else if (moment("3 PM", 'h A').isBefore(moment("9 AM", 'h A').add(i, 'h'))) {
+        arrayof$Times[i].addClass("future");
+    }
 }
+
+
+console.log(moment("10 AM", 'h A').add(2, 'h').format("h A"))
