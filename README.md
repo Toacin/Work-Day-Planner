549c101ca4929d68cc12df2472e0d253909b74b9
Toacin <toacinp@outlook.com>
2022-09-23 16:07:05 -0700

    finalized readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Work-Day-Planner
-Daily Planner
+# Work Day Planner
+
+## Description
+
+### **This repository includes all files of my Work-Day Planner**
+
+This work-day planner is designed to help users organize their their activities throughout the day. For ease-of-use, time blocks are automatically color coded to represent if it's corresponding time of day has already passed, is upcoming, or the present time of day. Moreover, any entries for a timeblock's input field will persist through refresh and closure of application once it's save button is clicked.
+
+### Developing the Application
+
+This biggest motivation for me to create this app was to learn about persistent data and Javascript optimization by use of use of JQuery. For the latter point, JQuery's object methods were deeply useful in applying quick dom manipulations and attribute data collection.
+
+Lastly, MomentJs was utilized for time functionality. MomentJs' extensive library of time formatting and current time behavior was extremely easy to use and its wide breadth of options made it very accessible with my application goals.
+
+### Future Development of this Application
+
+One feature that will be added in a future update is automatic deletion of previous day entries.
+
+---
+## Deployed Page link
+
+
+
+## Installation
+
+N/A (Please see above for deployed link.) Once on deployed page, begin adding entries and saving to see application in action!
+
+## Credits
+
+Special thanks to the UofA Coding Bootcamp teaching staff for guidance and assistance.
+
+## License
+
+Open Source
+
+---
+## Images of Application
+
+### Application Image
+![App Screenshot](./Assets/AppScreenshot.JPG "Screenshot of Application")
--- script.js
+++ script.js
@@ -1,30 +1,36 @@
+// grabs current Day for header
 const currentDay = moment().format("dddd, LL");
-// console.log(currentDay);
+// grabs where current day is on markup
 let $currentDay = $("#currentDay");
-// console.log(currentTime);
+// grabs all the save buttons
 let $button = $("button");
-arrayof$Times = [$("#9am"), $("#10am"), $("#11am"), $("#12pm"), $("#1pm"), $("#2pm"), $("#3pm"), $("#4pm"), $("#5pm")];
 
-arrayof$TimesID = [$("#9am").attr("id"), $("#10am").attr("id"), $("#11am").attr("id"), $("#12pm").attr("id"), $("#1pm").attr("id"), $("#2pm").attr("id"), $("#3pm").attr("id"), $("#4pm").attr("id"), $("#5pm").attr("id")];
+// array defined for all text inputs
+const arrayof$Times = [$("#9am"), $("#10am"), $("#11am"), $("#12pm"), $("#1pm"), $("#2pm"), $("#3pm"), $("#4pm"), $("#5pm")];
 
+// array defined for all text inputs' ID's
+const arrayof$TimesID = [$("#9am").attr("id"), $("#10am").attr("id"), $("#11am").attr("id"), $("#12pm").attr("id"), $("#1pm").attr("id"), $("#2pm").attr("id"), $("#3pm").attr("id"), $("#4pm").attr("id"), $("#5pm").attr("id")];
+
+// update Current Day on markup with the actual date
 $currentDay.text(currentDay);
 
+//this for loop does two things:
+// 1. this for loop will check each if current hour is greater than, equal to, or less than 9, and if so, it will make the 9am text input grey, red, or green. Next iteration will check against 10, and will change 10am element accordingly. Etc.
+// 2. It will set all input fields to what was stored in local data
 for (i=0; i<arrayof$Times.length; i++) {
-    if (moment("3 PM", 'h A').isAfter(moment("9 AM", 'h A').add(i, 'h'))) {
+    if (moment().hour() > (9+i)) {
         arrayof$Times[i].addClass("past");
-    } else if (moment("3 PM", 'h A').isSame(moment("9 AM", 'h A').add(i, 'h'))) {
+    } else if (moment().hour() == (9+i)) {
         arrayof$Times[i].addClass("present");
-    } else if (moment("3 PM", 'h A').isBefore(moment("9 AM", 'h A').add(i, 'h'))) {
+    } else if (moment().hour() < (9+i)) {
         arrayof$Times[i].addClass("future");
     }
     arrayof$Times[i].val(localStorage.getItem(arrayof$TimesID[i]));
 }
 
+// on button click, input's data will be set to local storage, using it's ID as a key and its value as the storage entry
 $button.click(function() {
     $currEntryAtt = $(this).siblings("input").attr("id");
-    console.log($currEntryAtt);
     $currEntry = $(this).siblings("input").val();
-    console.log($currEntry);
     localStorage.setItem($currEntryAtt, $currEntry);
-    console.log(localStorage.getItem("9am"));
-})
+});
